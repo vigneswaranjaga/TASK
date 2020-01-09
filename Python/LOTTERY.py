9153ec7067a9e35cb6602b3e292ad1a0d686b88d
vigneswaranjaga <43904744+vigneswaranjaga@users.noreply.github.com>
2020-01-09 02:15:24 +0000

    Update LOTTERY.py

--- Python/LOTTERY.py
+++ Python/LOTTERY.py
@@ -1,3 +1,7 @@
+"""
+ 1)Takes Users current date and time to say the Next Lottery draw 
+ 
+"""
 import datetime
 import time
 import calendar
@@ -7,7 +11,7 @@ now = datetime.datetime.now()
 
 day = time.strftime("%a")
 
-print("\n\n\t*** Welcome to Canadian National Lottery ***\n")
+print("\n\n******************** Welcome to Canadian National Lottery ********************\n")
 print("Lottery draw takes place twice per week, on Tuesday and Sunday at 9.30 pm")
 
 sunday = today + datetime.timedelta((calendar.SUNDAY-today.weekday()) % 7 )
@@ -15,14 +19,21 @@ tuesday = today + datetime.timedelta((calendar.TUESDAY-today.weekday()) % 7 )
 
 
 if now.hour <= 21 and now.minute <= 29 and now.second <= 59 and day == 'Tue':
-    print ("\n\tYour next Lottery draw is on : ", tuesday, "Tuesday at 9:30 PM")
+    print ("\nYour next Lottery draw is on : ", tuesday, "Tuesday at 9:30 PM")
 else :
-    print ("\n\tYour next Lottery draw is on : ", sunday, "Sunday at 9:30 PM")
+    print ("\nYour next Lottery draw is on : ", sunday, "Sunday at 9:30 PM")
     
-print("\n\n\t************************* Wish You Good Luck **********************************\n")
+print("\n\n******************** Wish You Good Luck ********************\n")
 
 
-userDate = input("\nEnter the date you want to purchase lottery(YYYY-MM-DD) : ")#format '2013-05-27'
+##################################################
+"""
+2)Get's User input both Date and Time. 
+Condition1: Check's for the day (Sunday or Tuesday)
+Condition2: If Sunday or Tuesday check's for the time < 9:30 pm
+"""
+
+userDate = input("\nEnter the date you want to purchase lottery(YYYY-MM-DD) : ")
 d = datetime.datetime.strptime(userDate, '%Y-%m-%d').date()
 
 userTime = input("\nEnter the Time you want to purchase lottery(HH:MM:SS) : ")
@@ -34,28 +45,24 @@ f = datetime.datetime.strptime(fixedTime,"%H:%M:%S").time()
 class lotterDraw:
     def next_weekday(self, d, weekday):
         days_ahead = weekday - d.weekday()
-        if days_ahead < 0 and t < f:
+        if days_ahead < 0:
             days_ahead += 7
-        elif days_ahead == 0:
-            if t < f:
-                days_ahead = 0
-            else:
-                days_ahead +=7
-       
+        elif days_ahead == 0 and t < f:
+            days_ahead = 0
+        elif days_ahead == 0 and t > f:
+            days_ahead += 7
+        else:
+            pass
         return d + datetime.timedelta(days_ahead)
 
 
 draw = lotterDraw()
-next_tuesday = draw.next_weekday(d, 1)
- # 0 = Monday, 1=Tuesday, 2=Wednesday...
-print(next_tuesday)
+next_tuesday = draw.next_weekday(d, 1) # 0 = Monday, 1=Tuesday, 2=Wednesday...
 next_sunday = draw.next_weekday(d, 6)
-print(next_sunday)
+
 
 if next_sunday < next_tuesday:
-    print("your next lottery Draw is on : ",next_sunday,"9:30 PM")
-        #print(" Time", f )
+    print("\nYour next lottery Draw is on : ",next_sunday,"9:30 PM")
     
 else: 
-    print("your next lottery Draw is on : ",next_tuesday,"9:30 PM")
-    #print(" Time", f )
+    print("\nYour next lottery Draw is on : ",next_tuesday,"9:30 PM")
