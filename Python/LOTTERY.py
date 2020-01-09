c1b991f60121f813701b34c35f67ecbd486fdc3c
vigneswaranjaga <43904744+vigneswaranjaga@users.noreply.github.com>
2020-01-09 01:21:41 +0000

    Update LOTTERY.py

--- Python/LOTTERY.py
+++ Python/LOTTERY.py
@@ -1,148 +1,61 @@
-'''
-from datetime import datetime
-from calendar import calendar
-
-current_date_time = datetime.now()
-current_day = datetime.today()
-print(current_date_time)
-print(current_day)
-'''
-'''
-from datetime import date
-today = date.today()
-# dd/mm/YY
-d1 = today.strftime("%d/%m/%Y")
-print("d1 =", d1)
-'''
-"""
-# Python program to Find day of 
-# the week for a given date 
-import calendar 
-
-print("******************** WELCOME TO LOTTERY**************************")
-print(
-user_choice = int (input("Do you wish to buy a lottery ticket? if so click 1 : "))
-if user_choice ==1:
-    
-def findDay(date): 
-	day, month, year = (int(i) for i in date.split('/'))	 
-	dayNumber = calendar.weekday(year, month, day) 
-	days =["Monday", "Tuesday", "Wednesday", "Thursday", 
-						"Friday", "Saturday", "Sunday"] 
-	return (days[dayNumber]) 
-
-# Driver program 
-date = '03 02 2019'
-print(findDay(date)) 
-
-"""
-
-'''
-##
-# Python's program to get last and coming Monday
- 
 import datetime
- 
-today = datetime.date.today()
-last_monday = today - datetime.timedelta(days=today.weekday()+1)
-coming_monday = today + datetime.timedelta(days=-today.weekday()+6, weeks=1)
-print("Today:", today)
-print("Last Monday:", last_monday)
-print("Coming Monday:", coming_monday)
-'''
-"""
-from datetime import date
-
-def satandsun(input):
-    d = input.toordinal()
-    last = d + 6
-    sunday = last + (last % 7)
-    #saturday = sunday + 6
-    print(date.fromordinal(sunday))
-    #print(date.fromordinal(saturday))
-
-
-satandsun(date(2020,1,7))
-"""
-"""
-import datetime
-today = datetime.date.today()
-today
-datetime.date(2013, 8, 13)
-idx = (today.weekday() + 1) % 7 # MON = 0, SUN = 6 -> SUN = 0 .. SAT = 6
-idx
-
-sun = today + datetime.timedelta(7+idx)
-sat = today + datetime.timedelta(7+idx-6)
-print("Last Sunday was {:%m/%d/%Y} and last Saturday was {:%m/%d/%Y}".format(sun, sat))
-#print(Last Sunday was 08/04/2013 and last Saturday was 08/10/2013)
-
-"""
-"""
-# Prints current date and time
-from datetime import datetime
-from calendar import calendar 
-
-date = datetime.today()
-print("******************** WELCOME TO LOTTERY**************************")
-user_choice = int (input("Do you wish to buy a lottery ticket? if so click 1 : "))
-if user_choice == 1:
-    print("Today date and time is ",date)
-"""
-"""
-import pendulum
-
-pendulum.now()
-
-#DateTime(2019, 4, 24, 17, 28, 13, 776007, tzinfo=Timezone('America/Los_Angeles'))
-pendulum.now().next(pendulum.SATURDAY)
-#Out[3]: DateTime(2019, 4, 27, 0, 0, 0, tzinfo=Timezone('America/Los_Angeles'))
-pendulum.now().next(pendulum.SATURDAY).strftime('%Y-%m-%d')
-
-
-"""
-import datetime
+import time
 import calendar
-today = datetime.date.today()#reference point.
-time = datetime.datetime.now()
+
+today = datetime.date.today()
+now = datetime.datetime.now()
+
+day = time.strftime("%a")
+
+print("\n\n\t*** Welcome to Canadian National Lottery ***\n")
+print("Lottery draw takes place twice per week, on Tuesday and Sunday at 9.30 pm")
+
 sunday = today + datetime.timedelta((calendar.SUNDAY-today.weekday()) % 7 )
 tuesday = today + datetime.timedelta((calendar.TUESDAY-today.weekday()) % 7 )
-Crt_time = time. strftime("%H:%M:%S")
-print("Current Time =",Crt_time)
-#print("current date and time ",time)
-print ("Next Sunday ",sunday)
-print ("Next Tuesday ",tuesday)
-
-#if 
-#if Crt_time <= 09:29:59 and 
-
-#saturday.weekday()
-#saturday = today + datetime.timedelta((5-today.weekday()) % 7 )
 
 
+if now.hour <= 21 and now.minute <= 29 and now.second <= 59 and day == 'Tue':
+    print ("\n\tYour next Lottery draw is on : ", tuesday, "Tuesday at 9:30 PM")
+else :
+    print ("\n\tYour next Lottery draw is on : ", sunday, "Sunday at 9:30 PM")
+    
+print("\n\n\t************************* Wish You Good Luck **********************************\n")
 
 
+userDate = input("\nEnter the date you want to purchase lottery(YYYY-MM-DD) : ")#format '2013-05-27'
+d = datetime.datetime.strptime(userDate, '%Y-%m-%d').date()
+
+userTime = input("\nEnter the Time you want to purchase lottery(HH:MM:SS) : ")
+t = datetime.datetime.strptime(userTime,"%H:%M:%S").time()
+
+fixedTime = ("21:30:00")
+f = datetime.datetime.strptime(fixedTime,"%H:%M:%S").time()
+
+class lotterDraw:
+    def next_weekday(self, d, weekday):
+        days_ahead = weekday - d.weekday()
+        if days_ahead < 0 and t < f:
+            days_ahead += 7
+        elif days_ahead == 0:
+            if t < f:
+                days_ahead = 0
+            else:
+                days_ahead +=7
+       
+        return d + datetime.timedelta(days_ahead)
 
 
+draw = lotterDraw()
+next_tuesday = draw.next_weekday(d, 1)
+ # 0 = Monday, 1=Tuesday, 2=Wednesday...
+print(next_tuesday)
+next_sunday = draw.next_weekday(d, 6)
+print(next_sunday)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if next_sunday < next_tuesday:
+    print("your next lottery Draw is on : ",next_sunday,"9:30 PM")
+        #print(" Time", f )
+    
+else: 
+    print("your next lottery Draw is on : ",next_tuesday,"9:30 PM")
+    #print(" Time", f )
